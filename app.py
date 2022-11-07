ba8cc3cb430c6c6f310ad4b0e5c409b7e71d8033
Timothy <46894223+timothy692@users.noreply.github.com>
2022-11-07 19:44:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,10 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+db = SQL(uri)
 
 db.execute("""CREATE TABLE IF NOT EXISTS history (
                 user_id INTEGER NOT NULL,
